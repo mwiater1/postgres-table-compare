ab6bd936ffca920ad461a5928fa50e26109f002f
Mateusz Wiater <mateuszwiater@gmail.com>
2024-11-26 00:02:51 -0500

    Initial commit.

---
+++ src/main/java/dev/mwiater/postgrestablecompare/PostgresTableCompareApplication.java
@@ -0,0 +1,13 @@
+package dev.mwiater.postgrestablecompare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostgresTableCompareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostgresTableCompareApplication.class, args);
+	}
+
+}
